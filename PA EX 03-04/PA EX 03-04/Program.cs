a747a860698531400e7070f2b8fc49033a6894b2
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-04-03 21:45:13 -0300

    PA EX 03-04 (2145)

--- PA EX 03-04/PA EX 03-04/Program.cs
+++ PA EX 03-04/PA EX 03-04/Program.cs
@@ -10,6 +10,14 @@ namespace PA_EX_03_04
     {
         static void Main(string[] args)
         {
+        
+        List<double> valores = new List<double>(){ 3,7,2, 4,6};
+
+        
+        List<double> novalista = valores.ConvertAll(e=> e/2);
+     
+        novalista.ForEach(e=> Console.WriteLine(e));
+            Console.ReadKey();
         }
     }
 }
