15fa651ab5d2de77a7968c0b039aa59d80d3efde
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-04-25 20:01:58 -0300

    Aula 25 04 2018 2001

---
+++ LINQ-EX01/LINQ-EX01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_EX01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota { get; set; }
+
+        public override string ToString()
+        {
+            return Nome + " -> " + Nota;
+        }
+    }
+}
