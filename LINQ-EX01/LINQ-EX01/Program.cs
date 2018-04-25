a2f1104f9de7353afcc8396fd57bcd324d5d6efd
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-04-25 20:06:55 -0300

    25-04-2018 2006

--- LINQ-EX01/LINQ-EX01/Program.cs
+++ LINQ-EX01/LINQ-EX01/Program.cs
@@ -10,6 +10,20 @@ namespace LINQ_EX01
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static List<Aluno> CriarAlunos()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            alunos.Add(new Aluno() { Nome = "Luis", Nota = 8.5 });
+            alunos.Add(new Aluno() { Nome = "José", Nota = 7.5 });
+            alunos.Add(new Aluno() { Nome = "Maria", Nota = 8.0 });
+            alunos.Add(new Aluno() { Nome = "Ana", Nota = 3.0 });
+            alunos.Add(new Aluno() { Nome = "Carol", Nota = 4.5 });
+            alunos.Add(new Aluno() { Nome = "Vanessa", Nota = 9.5 });
+
+            return alunos;
         }
     }
 
