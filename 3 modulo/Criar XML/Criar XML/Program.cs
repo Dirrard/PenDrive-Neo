f3b81ed65b0dc819b050ebcedb6a142de94360e9
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-05-02 19:13:58 -0300

    02-05-2018 1913

---
+++ 3 modulo/Criar XML/Criar XML/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criar_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string TelefoneResidencial { get; set; }
+        public string TelefoneCelular { get; set; }
+        public Endereco Endereco { get; set; }
+
+    }
+
+    class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
