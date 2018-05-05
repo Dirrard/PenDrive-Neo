32cb5bdcbcf856db264abad37c6863f8f07a9bb8
Dirrard <36750424+Dirrard@users.noreply.github.com>
2018-05-04 22:06:55 -0300

    Aula de teste dia 04-05-2018

---
+++ Aula de Teste/Test/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Teste;
+namespace Test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+      
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Pessoa p1 = new Pessoa();
+            p1.datansc = new DateTime(1999,1,12); 
+
+               Assert.IsTrue(p1.EhMaior());
+
+
+        }
+    }
+}
---
+++ Aula de Teste/Teste/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    public class Pessoa
+    {
+       public string nome;
+        public string sobrenome;
+        public DateTime datansc;
+
+        public bool EhMaior()
+        {
+            int idade = DateTime.Today.Year - this.datansc.Year;
+            if (idade >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
